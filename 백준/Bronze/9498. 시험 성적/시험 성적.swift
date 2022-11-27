3bf2fc251dc23eeed4bc79b05223d63ec82d7630
Isselgun <106157994+Isselgun@users.noreply.github.com>
2022-11-28 08:43:28 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.swift
@@ -0,0 +1,12 @@
+let i = Int(readLine()!)!
+if i >= 90 {
+	print ("A")
+}else if i >= 80 {
+    print ("B")
+}else if i >= 70 {
+    print ("C")
+}else if i >= 60 {
+    print ("D")
+}else {
+    print ("F")
+}
