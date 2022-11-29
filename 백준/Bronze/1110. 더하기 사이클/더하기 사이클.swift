9715d55302964a7fae2aa9a63c5ab4e2a3ac73ea
Isselgun <106157994+Isselgun@users.noreply.github.com>
2022-11-29 10:51:13 +0900

    [Bronze I] Title: 더하기 사이클, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.swift
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.swift
@@ -2,23 +2,15 @@ import Foundation
 
 var n = Int(readLine()!)!
 var originalValue = n
-var changeValue = 0
 var count = 0
 
-while n != changeValue {
-    changeValue = originalValue
-    changeValue = sum(originalValue)
-    originalValue = changeValue
+repeat{
     count += 1
-}
+    originalValue = sum(originalValue)
+}while n != originalValue
 
-if count == 0 {
-    print(1)
-}else {
-    print(count)
-}
+print(count)
 
 func sum(_ num: Int) -> Int {
-    // 뒤를 앞에 자리로     // 뒤를 합의 뒷자리로  // 조합한거
     num % 10 * 10 + (num / 10 + num % 10) % 10
 }
