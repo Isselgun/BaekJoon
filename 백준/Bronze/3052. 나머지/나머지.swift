b084471d50500951c01ff9780d3b9a98a2293137
Isselgun <106157994+Isselgun@users.noreply.github.com>
2022-11-29 11:59:21 +0900

    [Bronze II] Title: 나머지, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.swift
+++ 백준/Bronze/3052. 나머지/나머지.swift
@@ -1,12 +1,5 @@
-import Foundation
-
-var a: [Int] = []
-
+var a: Set<Int> = []
 for _ in 1...10 {
-    let value = Int(readLine()!)!
-    if !a.contains(value % 42) {
-        a.append(value % 42)
-    }
+    a.insert(Int(readLine()!)! % 42)
 }
-
 print(a.count)
